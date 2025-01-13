a17f26d894eb389c213d3313202d25e888496d4a
Dan Hossom <dewrgleision@gmail.com>
2025-01-13 21:48:05 +0100

    add :verified_routes to plug

--- lib/phoenix_voip_test_web/plugs/auth.ex
+++ lib/phoenix_voip_test_web/plugs/auth.ex
@@ -1,13 +1,13 @@
 defmodule PhoenixVoipTestWeb.Plugs.Auth do
+  use PhoenixVoipTestWeb, :verified_routes
   import Plug.Conn
   import Phoenix.Controller
-  alias PhoenixVoipTest.Accounts
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
     if user_id = get_session(conn, :user_id) do
-      user = Accounts.get_user!(user_id)
+      user = PhoenixVoipTest.Accounts.get_user!(user_id)
       assign(conn, :current_user, user)
     else
       assign(conn, :current_user, nil)
